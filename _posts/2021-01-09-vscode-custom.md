0041de4d2b59dab96860b253ac50864ce456fdde
closetotheworld <remantion@nate.com>
2021-01-09 22:44:35 +0900

    2021-01-09 wsl2 and vscode custom post

---
+++ _posts/2021-01-09-vscode-custom.md
@@ -0,0 +1,31 @@
+---
+title: "VScode extension - material theme (vscode custom)"
+
+categories:
+  - etc
+tags :
+  - [vscode, custom, tutorial]
+
+toc: true
+toc_sticky: true
+
+last_modified_at : 2021-01-09T21:40:00
+---
+
+### VS code!
+- 내가 가장 좋아하는 에디터이다.
+- 일반 테마보다 예쁜 테마와 아이콘을 사용해보고자 커스텀 하는 법을 검색한 내용을 정리한다.
+
+### material theme
+- vscode extension을 click ! 이후 material theme을 검색하면 이와 같은 테마가 나온다.
+    ![image](https://user-images.githubusercontent.com/37994634/104091848-7467eb80-52c3-11eb-80d3-eefeece3db4a.png){: width : 300 height: 300}
+
+- extension을 설치한 후 f1을 입력하여 material theme을 적용하면 끝! 본인은 기본테마를 사용한다 . . !
+
+### material Icon theme
+- material theme에 맞게 아이콘들도 material design으로 바꾸어 준다 ! 벌써 갓-개발자가 된 느낌이다.
+- extension에 Material Icon Theme을 검색하면 나온다 !
+    ![image](https://user-images.githubusercontent.com/37994634/104091888-b133e280-52c3-11eb-8b09-b87112eaf2b9.png){: width : 300 height : 300}
+
+
+  
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -16,4 +16,40 @@ var store = [{
         "tags": ["language","tutorial"],
         "url": "https://closetotheworld.github.io/golang/post3/",
         "teaser": null
+      },{
+        "title": "2021-01-06",
+        "excerpt":"오랜만에 집을 왔다 얼마만에 집을 온건지, 추석 이후로 오랜만에 본가에 왔다. 가족들의 얼굴을 오랜만에 보니 너무 좋다. 작년 하반기에는 취업준비니 개인 프로젝트니 공부니 나 자신을 너무 채찍질 했던 것 같다. 그래도 주변 사람과 가족은 챙기면서 살아야지. 다잡은 목표 취업준비를 열심히 하되 나를 놓지말자. 내가 좋아하는 것, 옷이나 클래식음악 등등 쉴...","categories": ["daily"],
+        "tags": ["diary"],
+        "url": "https://closetotheworld.github.io/daily/diary/",
+        "teaser": null
+      },{
+        "title": "2021-01-07",
+        "excerpt":"집에 오니 너무 편하당     일단 잘 먹는다   부모님 얼굴 매일보니 좋다   평화롭다   study     Golang 실습코드 만드느라 포스팅은 몰아서 해야겠다   CS공부한것도 하나씩 올려야지   아무래도 본가에 와서 노트북만 들고 하려니 더블모니터가 그리워잇      오늘도 고생했다!   ","categories": ["daily"],
+        "tags": ["diary"],
+        "url": "https://closetotheworld.github.io/daily/diary/",
+        "teaser": null
+      },{
+        "title": "2021-01-08",
+        "excerpt":"오늘 먹은 것        뒷고기는 항상 옳으시다    내일은 다시 구미로     다시 가서 열심히 포스트하고 열심히 공부하고   얼마 남지않은 연구실에서의 공부 야무지게 끝내자잇   오늘도 고생했따!  ","categories": ["daily"],
+        "tags": ["diary"],
+        "url": "https://closetotheworld.github.io/daily/diary/",
+        "teaser": null
+      },{
+        "title": "Golang, 간단히 살펴보자!- Go series(2)",
+        "excerpt":"학습 한 내용     Go의 흐름   Golang의 실행흐름  Golang의 실행흐름을 알아보자     main.go 에서 보통 시작한다.      # main.go   import (    \"log\"    \"os\"  )   func init(){    log.setOutput(os.Stdout)  }   func main(){    search.Run(\"wonryang\")  }              main 이라는 패키지에 구현되어있다.   Go의 모든 코드는 패키지에 종속되어야 한다.   ","categories": ["Golang"],
+        "tags": ["language","tutorial"],
+        "url": "https://closetotheworld.github.io/golang/go_series_2/",
+        "teaser": null
+      },{
+        "title": "VScode extension - material theme (vscode custom)",
+        "excerpt":"VS code! 내가 가장 좋아하는 에디터이다. 일반 테마보다 예쁜 테마와 아이콘을 사용해보고자 커스텀 하는 법을 검색한 내용을 정리한다. material theme vscode extension을 click ! 이후 material theme을 검색하면 이와 같은 테마가 나온다. extension을 설치한 후 f1을 입력하여 material theme을 적용하면 끝! 본인은 기본테마를 사용한다 . . ! material Icon theme...","categories": ["etc"],
+        "tags": ["vscode","custom","tutorial"],
+        "url": "https://closetotheworld.github.io/etc/vscode-custom/",
+        "teaser": null
+      },{
+        "title": "WSL2 terminal custom",
+        "excerpt":"WSL2? Windows Subsystem Linux 윈도우 시스템을 Linux로도 이용할 수 있는 것! WSL1보다 2가 조금 더 성능이 좋다고 한다. 리눅스의 terminal을 좋아하는 나로서는 너무 환영! terminal을 바꿔보자 MAC에서도 사용하는 oh my zsh를 설치할 것이다. 조금 더 예쁜 터미널 .. 완성이 되면 개발이 더 하고싶어질 거에요! 우선 먼저 zsh를 다운받아야 합니다..! zsh...","categories": ["etc"],
+        "tags": ["wsl2","terminal","custom","tutorial"],
+        "url": "https://closetotheworld.github.io/etc/wsl2-custom/",
+        "teaser": null
       }]
